f802eeff67d6e92e56287d713547e6aae91db9fc
shekharbora <chandu42011@gmail.com>
2015-01-02 17:13:42 +0530

    make it better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,7 @@ class HomeController < ApplicationController
 		@search=Decument.select('decuments.*').where("decuments.title like ? OR decuments.subject like?", "%#{params[:search_p]}%","%#{params[:search_p]}%")
 		respond_to do |format|
 	      format.js {  }
+	       format.html {  }
 	   end	
 	end
 end
